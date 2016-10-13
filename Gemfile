dd230ea430d1807def02a8a2347dd6c16b1dfabe
seo-naotoshi <naotoshi.seo@dena.com>
2016-10-13 18:13:42 +0900

    Update example/example.rb

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source "http://rubygems.org"
 
-gem 'triglav_client', path: '..'
+gemspec
 gem 'pry-nav'
--- example/example.rb
+++ example/example.rb
@@ -1,35 +1,40 @@
 require 'triglav_client'
 
-TriglavClient.configure do |config|
+config = TriglavClient::Configuration.new do |config|
   uri = URI.parse('http://localhost:3000')
   config.scheme = uri.scheme
   config.host = "#{uri.host}:#{uri.port}"
 end
-auth_api = TriglavClient::AuthApi.new
-resources_api = TriglavClient::ResourcesApi.new
+api_client = TriglavClient::ApiClient.new(config)
 
 begin
+  auth_api = TriglavClient::AuthApi.new(api_client)
   credential = TriglavClient::Credential.new(name: 'triglav_test', password: 'triglav_test', authenticator: 'local')
   result = auth_api.create_token(credential)
   if result
     access_token = result.access_token
-   #  TriglavClient.configure do |config|
-   #    config.api_key['Authorization'] = access_token
-   #  end
+    api_client.config.api_key['Authorization'] = access_token
   end
   puts "me: #{auth_api.me}"
 rescue TriglavClient::ApiError => e
-  require 'pry'
-  binding.pry
-  puts "Exception when calling AuthApi->create_token: #{e}"
+  if e.code == 0
+    puts "Could not connect"
+  else
+    puts "Exception when calling AuthApi->create_token: #{e}"
+  end
 end
 
 begin
+  resources_api = TriglavClient::ResourcesApi.new(api_client)
   cluster_name = 'vertica'
   consumable = true
   notifiable = true
   result = resources_api.list_resources(cluster_name, consumable, notifiable)
   puts "list_resources(vertica): #{result}"
 rescue TriglavClient::ApiError => e
-  puts "Exception when calling AuthApi->create_token: #{e}"
+  if e.code == 0
+    puts "Could not connect"
+  else
+    puts "Exception when calling AuthApi->create_token: #{e}"
+  end
 end
